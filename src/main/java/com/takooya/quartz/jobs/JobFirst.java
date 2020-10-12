973a11015040fd2e229e2e14d8a5c628ff86c1d4
takooya <takooya@126.com>
2020-10-12 15:39:30 +0800

    使结构慢慢清晰

---
+++ src/main/java/com/takooya/quartz/jobs/JobFirst.java
@@ -0,0 +1,39 @@
+package com.takooya.quartz.jobs;
+
+import com.takooya.quartz.QuartzJobLauncher;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.CronTrigger;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.impl.JobDetailImpl;
+import org.quartz.impl.triggers.CronTriggerImpl;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+
+@Slf4j
+@Component
+public class JobFirst {
+    private String jobName = "job1";
+    private String cronExpression = "0/5 * * * * ?";
+    private String triggerName = "cronTrigger1";
+
+    public JobDetail job1() {
+        JobDetailImpl jobDetailImpl = new JobDetailImpl();
+        jobDetailImpl.setJobClass(QuartzJobLauncher.class);
+        jobDetailImpl.setName(jobName);
+        jobDetailImpl.setDurability(true);
+        JobDataMap timeout = new JobDataMap();
+        timeout.put("timeout", 10);
+        jobDetailImpl.setJobDataMap(timeout);
+        return jobDetailImpl;
+    }
+
+    public CronTrigger cronTrigger1() throws ParseException {
+        CronTriggerImpl cronTrigger = new CronTriggerImpl();
+        cronTrigger.setJobName(jobName);
+        cronTrigger.setCronExpression(cronExpression);
+        cronTrigger.setName(triggerName);
+        return cronTrigger;
+    }
+}
